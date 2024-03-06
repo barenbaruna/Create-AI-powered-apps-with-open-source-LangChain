a40879f5d2126723a5ec07747711eb90d16ae9d8
Baren Baruna Harahap <barenbarunaharahap@gmail.com>
2024-03-06 15:27:13 +0700

    Update simple_llm.py

--- simple_llm.py
+++ simple_llm.py
@@ -3,7 +3,7 @@ from langchain_openai import ChatOpenAI
 import gradio as gr
 
 # Memasukkan API key
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = "YOUR API KEY"
 
 gpt3 = ChatOpenAI(model_name="gpt-3.5-turbo" )
 

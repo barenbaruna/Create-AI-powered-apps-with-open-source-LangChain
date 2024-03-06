28459ca8ac37ea4bc6e87ffe37d06b3d57e82e06
Baren Baruna Harahap <barenbarunaharahap@gmail.com>
2024-03-06 15:27:44 +0700

    Update prompt_with_template.py

--- prompt_with_template.py
+++ prompt_with_template.py
@@ -3,7 +3,7 @@ from langchain.prompts import PromptTemplate
 import os
 from langchain_openai import ChatOpenAI
 
-openai_api_key = "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"
+openai_api_key = "YOUR API KEY"
 os.environ["OPENAI_API_KEY"] = openai_api_key
 
 # Mendefinisikan model AI
